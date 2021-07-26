a4d2f28ffd411e663991e7b18615cc070f1098a8
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-07-26 20:22:42 +0200

    Migrating pokedex project to Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section id="main-display">
+    <div id="logo">
+      <img
+        src="./assets/imgs/Pokemon_logo.png"
+        alt="Pokemon Logo"
+        id="main-logo"
+      />
+      <img src="./assets/imgs/pokeball.png" alt="Pokeball" id="pokeball" />
+    </div>
+  </section>
+  <section id="pokemon-display">
+    <div
+      v-for="(pokemon, index) in poke"
+      :key="pokemon"
+      class="card"
+      :id="pokemon.name"
+    >
+      <p>{{ pokemon.name }}</p>
+      <p>{{ index }}</p>
+      <p>{{ pokemon.url }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  created() {
+    axios.get(this.pokeApi).then((response) => {
+      /* console.log(
+          response.data.results
+        );
+        Array containing all pokemons specifics urls*/
+      this.poke = [...response.data.results];
+      // console.log(this.poke);
+      for (let i = 1; i < 152; i++) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+          .then((response) => response.json())
+          .then((results) => {
+            this.pokemonsData.push({
+              name: results.name,
+              index: i,
+            });
+          });
+      }
+      // this.poke.forEach((data) => {
+      //   axios.get(data.url).then((result) => {
+      //     // console.log(data.url);
+      //     this.pokemonsData.push({
+      //       name: result.data.name,
+      //     });
+      //   });
+      // });
+      console.log(this.pokemonsData);
+    });
+  },
+  data() {
+    return {
+      pokeApi: "https://pokeapi.co/api/v2/pokemon/?limit=151&?order=1",
+      specific: "",
+      pokemonsData: [],
+      poke: [],
+      // pokeUrls: this.pokemons.url,
+    };
+  },
+  computed: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("./styles/styles.css");
 </style>
